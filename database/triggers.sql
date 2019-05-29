9f792b59a53a58ac62ba56542f57864600138ca4
Juan Menjivar <jmmenjivaro@gmail.com>
2019-05-29 13:42:25 -0600

    mas cambios

--- database/triggers.sql
+++ database/triggers.sql
@@ -4,7 +4,7 @@
 BEGIN
 DECLARE tot varchar(200);
 SET tot=(new.qty)*(new.price);
-INSERT INTO peps VALUES(new.id, CURRENT_TIMESTAMP(),new.name,new.qty,new.price,tot);
+INSERT INTO peps_compras VALUES(new.id, CURRENT_TIMESTAMP(),new.name,new.qty,new.price,tot);
 END;
 
 -- pepsComprasUpdate
